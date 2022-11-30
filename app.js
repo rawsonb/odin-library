59eb134aa1f9b8b293413d3817ad1b5f1b6703b4
f.blackmon <f.rawsonblackmon@gmail.com>
2022-11-30 15:36:42 -0500

    oops

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 let library = [];
 
+const addButton = document.querySelector("#add-button");
+addButton.addEventListener("click", addClicked, false);
+
+function addClicked(event){
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    if(title.value && pages.value && author.value){
+        addBook(title.value, pages.value, author.value);
+    }
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    event.preventDefault();
+}
+
 function Book(name, pages, author) {
     this.name = name;
     this.pages = pages;
     this.author = author;
+    //this.element;
 }
 
-function addBook() {
-    const name = prompt("Book name:");
-    const pages = prompt("Number of pages:");
-    const author = prompt("Book author:");
+function addBook(name, pages, author) {
     library.push(new Book(name, pages, author));
     updateDisplay(library);
 }
@@ -22,14 +36,21 @@ function updateDisplay(library) {
         const title = document.createElement("h2");
         const author = document.createElement("p");
         const pages = document.createElement("p");
+        const deleteButton = document.createElement("button");
+
+        deleteButton.setAttribute("onclick", `library.splice(${x},1);updateDisplay(library);`)
 
         title.textContent = library[x].name;
         author.textContent = "by " + library[x].author;
         pages.textContent = library[x].pages + " pages";
+        deleteButton.textContent = "X";
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
+        card.appendChild(deleteButton);
+
+        //library[x].element = x;
 
         books.appendChild(card);
     }
